8adc978648c55659b9e4b9a642e1c1c71ed03049
Marcel Rico <personal.mr4@gmail.com>
2019-12-14 13:20:21 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
-
+  if block_given?
+    idx = 0
+    block_return_values = []
+    while collection[idx]
+      block_return_values << yield(collection[idx])
+      idx+=1
+    end
+    return !block_return_values.include?(false)
+  end
+  false
 end
