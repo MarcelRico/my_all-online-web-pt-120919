601444fcc6f1e280e6bccda63bfbdc7b378fd7b3
Marcel Rico <personal.mr4@gmail.com>
2019-12-14 13:18:26 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,7 +5,8 @@ def my_all?(collection)
     idx = 0
     block_return_values = []
     while collection[idx]
-      yield(collection[idx])
+      block_return_values << yield(collection[idx])
       idx+=1
     end
+  end
 end
